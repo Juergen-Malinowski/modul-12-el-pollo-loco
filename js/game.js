812c773ea51079ca120a139e6b50dde8687e4aa2
Jürgen Malinowski <jm-malle@gmx.de>
2025-10-14 12:42:15 +0200

    New JS-classes created

--- js/game.js
+++ js/game.js
@@ -1,8 +1,19 @@
 let canvas;
-let character = new Image();
+let ctx;
+let character = new Character();
 
 
 function init() {
+    //grundsätzliche Einbindung für canvas und Darstellungsart (2D/3D) ...
     canvas = document.getElementById("canvas");
-    character.src = "./assets/img/2_charakter_pepe/2_walk/W-21.png";
+    ctx = canvas.getContext("2d");                  // im 2D-Format
+
+    console.log("My Charakter: ", character);
+    
+
+    // Charakter-Bild laden ... dauert etwas, weshalb "ctx.drawImage" in eine TIMEOUT-Funktion gepackt wird ...
+    // character.src = "../assets/img/2_charakter_pepe/2_walk/W-21.png";
+    // setTimeout(function () {   // setzt den TIMEOUT, damit das PNG Zeit zum Laden hat ...
+    //     ctx.drawImage(character, 20, 20, 50, 150);
+    // }, 1000);
 }
---
+++ js/models-classes/character.class.js
@@ -0,0 +1,10 @@
+
+// mit "extends MovableObject" wird die Klasse "character" von der 
+// Klasse "MovableObject" abgeleitet ...
+
+class Character extends MovableObject {
+
+    jump() {
+
+    }
+}
