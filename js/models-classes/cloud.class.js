ca5daf48fd4d21f67f0930d75d1bb6761b7bf012
Jürgen Malinowski <jm-malle@gmx.de>
2025-10-15 14:32:08 +0200

    now clounds are moving left

--- js/models-classes/cloud.class.js
+++ js/models-classes/cloud.class.js
@@ -8,6 +8,16 @@ class Cloud extends MovableObject {
         this.x = 50 + Math.random() * 150; 
         this.y = 0 + Math.random() * 50;
         this.width = 400 + Math.random() * 100;
+        this.animate();
     }
+
+    // Wolken bewegen ...
+    animate() {
+        setInterval(() => {
+            this.x -= 0.2;     // Modifikation der x-Position (Wolke bewegt sich nach links)
+
+        // (17ms = 0,017s bzw. 60 Frames pro Sek. bzw. 1000/60 = 17 ms / 60 Frames pro Sek. ist so das Maximum in Spielen)
+    }, 1000 / 60);             // Intervall in ms (17 ms hier), in der die Wolke neu gezeichnet wird 
+    };
 }
 
