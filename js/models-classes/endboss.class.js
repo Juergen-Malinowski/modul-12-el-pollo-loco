0198c7bf3cca02133839154def62180662b96777
Jürgen Malinowski <jm-malle@gmx.de>
2025-10-27 10:45:54 +0100

    New images of the final boss added

--- js/models-classes/endboss.class.js
+++ js/models-classes/endboss.class.js
@@ -12,7 +12,16 @@ class Endboss extends MovableObject {
         right: 20,
     }
 
+
     imagesWalking = [
+        '../assets/img/4_feinde_boss_huhn/1_walk/G1.png',
+        '../assets/img/4_feinde_boss_huhn/1_walk/G2.png',
+        '../assets/img/4_feinde_boss_huhn/1_walk/G3.png',
+        '../assets/img/4_feinde_boss_huhn/1_walk/G4.png',
+    ];
+
+
+    imagesAlert = [
         '../assets/img/4_feinde_boss_huhn/2_alert/G5.png',
         '../assets/img/4_feinde_boss_huhn/2_alert/G6.png',
         '../assets/img/4_feinde_boss_huhn/2_alert/G7.png',
@@ -21,6 +30,23 @@ class Endboss extends MovableObject {
         '../assets/img/4_feinde_boss_huhn/2_alert/G10.png',
         '../assets/img/4_feinde_boss_huhn/2_alert/G11.png',
         '../assets/img/4_feinde_boss_huhn/2_alert/G12.png',
+    ];
+
+    imagesAttack = [
+        '../assets/img/4_feinde_boss_huhn/3_attack/G13.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G14.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G15.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G16.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G17.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G18.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G19.png',
+        '../assets/img/4_feinde_boss_huhn/3_attack/G20.png',
+    ];
+
+    imagesHurt = [
+        '../assets/img/4_feinde_boss_huhn/4_hurt/G21.png',
+        '../assets/img/4_feinde_boss_huhn/4_hurt/G22.png',
+        '../assets/img/4_feinde_boss_huhn/4_hurt/G23.png',
     ];
 
     constructor() {
