905a18016cd33bd50892a61ea2f29ab9760d2d39
Jürgen Malinowski <jm-malle@gmx.de>
2025-10-29 11:32:54 +0100

    Start screen further designed; game start established via START button

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+    // Startbildschirm ausblenden
+    document.getElementById('startScreen').style.display = 'none';
+
+    // Canvas sichtbar machen
+    document.getElementById('canvas').style.display = 'block';
+
+    // === SPIEL STARTEN ===
+    initLevel();   // Level-Daten laden
+    init();        // Welt, Charakter, Steuerung etc. aufbauen
+}
