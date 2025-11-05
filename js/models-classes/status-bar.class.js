47c142b3788fa069fe35106627cc80ba0d5644c3
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-05 16:44:39 +0100

    The character's status bar now displays a whole segment shortly before the end (previously already ‘empty’).

--- js/models-classes/status-bar.class.js
+++ js/models-classes/status-bar.class.js
@@ -87,7 +87,7 @@ class StatusBar extends DrawableObjects {
         else if (this.percentage >= 80) return 1;
         else if (this.percentage >= 60) return 2;
         else if (this.percentage >= 40) return 3;
-        else if (this.percentage >= 20) return 4;
+        else if (this.percentage >= 1) return 4;
         else return 5;
     }
 }
